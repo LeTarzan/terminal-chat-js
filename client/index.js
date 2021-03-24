87dbbed15a3cf66cce551f0f99ada52c8b5a583e
letarzan <leandrorevolve@gmail.com>
2021-03-23 21:37:16 -0300

    CLI finished

--- client/index.js
+++ client/index.js
@@ -1,7 +1,16 @@
+/* 
+  node index.js --username letarzan --room salinha --hostUri localhost
+*/
+
 import Events from 'events';
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
 
 const componentEmitter = new Events()
+const [nodePath, filePath, ...args] = process.argv
+const config = CliConfig.parseArguments(args)
+
+console.log(config)
 
 const controller = new TerminalController()
 await controller.initializeTable(componentEmitter)
---
+++ client/src/cliConfig.js
@@ -0,0 +1,30 @@
+export default class CliConfig {
+  constructor({ userName, hostUri, room }) {
+    this.userName = userName
+    this.room = room
+
+    const {hostname, port, protocol} = new URL(hostUri)
+    this.hostname = hostname
+    this.port = port
+    this.protocol = protocol.replace(/\W/, '') // anything that isn't letter 
+  }
+
+  static parseArguments(args) {
+    const cmd = new Map()
+
+    for (const key in args) {
+      const COMMAND_PREFFIX = '--'
+      const index = parseInt(key)
+      const command = args[key]
+
+      if (!command.includes(COMMAND_PREFFIX)) continue
+
+      cmd.set(
+        command.replace(COMMAND_PREFFIX, ''),
+        args[index + 1]
+      )
+    }
+
+    return new CliConfig(Object.fromEntries(cmd))
+  }
+}
